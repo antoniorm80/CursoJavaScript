d3f2791625b6b8893813707c8d304e02658e7218
Antonio Rivera <antoniorm80@hotmail.com>
2020-09-16 16:27:38 -0500

    Agregando carpeta AsyncAwait

---
+++ src/AsyncAwait/callback.js
@@ -0,0 +1,25 @@
+// Llamadas asíncronas.  CallBack Hell
+function reuestHandler(req, res) {
+    User.findById(req.userId, function(err, user) {
+        if(err) {
+            res.send(err);
+        } else {
+            Tasks.findById(user.taskId), function(err, tasks) {
+                if (err) {
+                    return res.send(err)
+                }else {
+                    tasks.completed = true;
+                    tasks.save(function(err) {
+                        if (err) {
+                            return res.send(err);
+                        }else {
+                            res.send('Task Completed')
+                        }
+                    })
+                }
+            }
+        }
+    })
+    // Whatever Code
+
+}
