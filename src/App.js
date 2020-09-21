ab5432508a9ec26eab9660a073056e72cdada2bd
Antonio Rivera <antoniorm80@hotmail.com>
2020-09-21 11:35:19 -0500

    Validando conexion a GitHub

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React -Quién como tu
         </a>
       </header>
     </div>
